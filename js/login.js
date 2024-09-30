2f5c97361f94b27b88a66a74e97cbafa3868d3a0
Julian Fernando Camacho Calle <u22242383@utp.edu.pe>
2024-09-29 21:33:07 -0500

    login

---
+++ js/login.js
@@ -0,0 +1,12 @@
+const registerButton = document.getElementById('register');
+const loginButton = document.getElementById('login');
+const contenedor = document.getElementById('contenedor');
+
+// Eventos para cambiar entre registro e inicio de sesión
+registerButton.addEventListener('click', () => {
+    contenedor.classList.add('active');
+});
+
+loginButton.addEventListener('click', () => {
+    contenedor.classList.remove('active');
+});
