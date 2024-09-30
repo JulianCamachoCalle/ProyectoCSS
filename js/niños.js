d0337e2d151d45cd48217d77ce6c9de6eae9f94d
walter046 <165204329+walter046@users.noreply.github.com>
2024-09-30 03:32:41 -0500

    areado

--- js/niños.js
+++ js/niños.js
@@ -13,3 +13,20 @@ function rotateImages() {
 
 // Cambiar imagen cada 3 segundos
 setInterval(rotateImages, 3000);
+
+const imagenes = document.querySelectorAll('.imagen-ninos-shop');
+
+imagenes.forEach((imagen) => {
+    const originalSrc = imagen.src; // Guardamos la ruta original de la imagen
+    const hoverSrc = imagen.getAttribute('data-hover'); // Obtenemos la imagen de hover desde el atributo data-hover
+
+    // Cambiamos la imagen cuando el cursor está sobre ella
+    imagen.addEventListener('mouseenter', () => {
+        imagen.src = hoverSrc;
+    });
+
+    // Restauramos la imagen original cuando el cursor sale de la imagen
+    imagen.addEventListener('mouseleave', () => {
+        imagen.src = originalSrc;
+    });
+});
